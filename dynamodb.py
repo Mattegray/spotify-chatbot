6bfaaf8152d51c1ee993e46283548b98d8412cc3
Mattegray <hyungwoo_128@outlook.com>
2020-08-17 23:14:59 +0900

    Added more lines of code

--- dynamodb.py
+++ dynamodb.py
@@ -1,11 +1,61 @@
 import sys
 import os
 import boto3
+import requests
+import base64
+import json
+import logging
+import pymysql
+
+client_id = "0e757e773b7a40dfbdf63381d3169ddf"
+client_secret = "59774097106141119b3ba074c69b99c9"
+
+host = "fastcampus.crqhm4yzezxw.us-east-2.rds.amazonaws.com"
+port = 3306
+username = "matt"
+database = "production"
+password = "matt1234"
 
 def main():
 
     try:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
+        dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', endpoint_url='http://dynamodb.ap-northeast-2.amazonaws.com')
+    except:
+        logging.error('could not connect to dynamodb')
+        sys.exit(1)
+
+    try:
+        conn = pymysql.connect(host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
+        cursor = conn.cursor()
+    except:
+        logging.error("could not connect to rds")
+        sys.exit(1)
+
+    headers = get_headers(client_id, client_secret)
+
+
+def get_headers(client_id, client_secret):
+
+    endpoint = "https://accounts.spotify.com/api/token"
+    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
+
+    headers = {
+        "Authorization": "Basic {}".format(encoded)
+    }
+
+    payload = {
+        "grant_type": "client_credentials"
+    }
+
+    r = requests.post(endpoint, data=payload, headers=headers)
+
+    access_token = json.loads(r.text)['access_token']
+
+    headers = {
+        "Authorization": "Bearer {}".format(access_token)
+    }
+
+    return headers
 
 if __name__=='main':
     main()
