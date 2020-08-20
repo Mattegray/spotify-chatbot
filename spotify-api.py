560f765ae46113634a4f9261de7bdea7402d728c
Mattegray <hyungwoo_128@outlook.com>
2020-08-20 15:22:50 +0900

    Added and use pymysql library

--- spotify-api.py
+++ spotify-api.py
@@ -3,12 +3,32 @@ import requests
 import base64
 import json
 import logging
+import pymysql
 
 client_id = "0e757e773b7a40dfbdf63381d3169ddf"
 client_secret = "59774097106141119b3ba074c69b99c9"
 
+host = "fastcampus.crqhm4yzezxw.us-east-2.rds.amazonaws.com"
+port = 3306
+username = "matt"
+database = "production"
+password = "matt1234"
+
 
 def main():
+    try:
+        conn = pymysql.connect(host, user=username, passwd=password, db=database, use_unicode=True, charset='utf8')
+        cursor = conn.cursor()
+    except:
+        logging.error("Could not connect to RDS")
+        sys.exit(1)
+
+    cursor.execute("SHOW TABLES")
+    print(cursor.fetchall())
+
+    print("Success")
+    sys.exit(0)
+
     headers = get_headers(client_id, client_secret)
 
     params = {
