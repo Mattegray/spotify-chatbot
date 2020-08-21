83158e64fba33af61cd552c4f2752e8590c5c882
Mattegray <hyungwoo_128@outlook.com>
2020-08-22 00:11:47 +0900

    Get top-tracks and audio-features from spotify into parquet format import to S3

--- s3-conn.py
+++ s3-conn.py
@@ -7,6 +7,8 @@ import base64
 import json
 import pymysql
 from datetime import datetime
+import pandas as pd
+import jsonpath
 
 
 client_id = "0e757e773b7a40dfbdf63381d3169ddf"
@@ -22,7 +24,7 @@ password = "matt1234"
 '''
 Get artist_id from AWS RDS
 Fetch data from Spotify using API
-Get json format
+List of dictionaries
 Import to S3
 '''
 
@@ -37,21 +39,67 @@ def main():
 
     headers = get_headers(client_id, client_secret)
 
-    cursor.execute("SELECT id from artists")
+    # Get artist_id from AWS RDS
+    cursor.execute("SELECT id from artists LIMIT 10")
+
+    # Get top-tracks from Spotify for each artist
+    top_track_keys = {
+        'id': 'id',
+        'name': 'name',
+        'popularity': 'popularity',
+        'external_url': 'external_url.spotify'
+    }
+    top_tracks = []
+
+    for (id, ) in cursor.fetchall():
+        URL = "https://api.spotify.com/v1/artists/{}/top-tracks".format(id)
+        params = {
+            'country': 'US'
+        }
+        r = requests.get(URL, params=params, headers=headers)
+        raw = json.loads(r.text)
+
+        for i in raw['tracks']:
+            top_track = {}
+            for k, v in top_track_keys.items():
+                top_track.update({k: jsonpath.jsonpath(i, v)})
+                top_track.update({'artist_id': id})
+                top_tracks.append(top_track)
+
+    # Extract track ids
+    track_ids = [i['id'][0] for i in top_tracks]
+
+    top_tracks = pd.DataFrame(top_tracks)
+    top_tracks.to_parquet('top-tracks.parquet', engine='pyarrow', compression='snappy')
 
     dt = datetime.utcnow().strftime("%Y-%m-%d")
-    print(dt)
-    sys.exit(0)
-
-    with open('top_tracks.json', 'w') as f:
-        for i in top_tracks:
-            json.dump(i, f)
-            f.write(os.linesep)
 
     s3 = boto3.resource('s3')
-    object = s3.Object('matt-spotify-artists', 'dt={}/top-tracks.json'.format(dt))
-    data = open('top-tracks.json', 'rb')
+    object = s3.Object('matt-spotify-artists', 'top-tracks/dt={}/top-tracks.parquet'.format(dt))
+    data = open('top-tracks.parquet', 'rb')
     object.put(Body=data)
+
+    # Get audio-features for each track
+    tracks_batch = [track_ids[i: i+100] for i in range(0, len(track_ids), 100)]
+
+    audio_features = []
+    for i in tracks_batch:
+        ids = ','.join(i)
+        URL = "https://api.spotify.com/v1/audio-features/?ids={}".format(ids)
+        r = requests.get(URL, headers=headers)
+        raw = json.loads(r.text)
+        audio_features.extend(raw['audio_features'])
+
+    audio_features = pd.DataFrame(audio_features)
+    audio_features.to_parquet('audio-features.parquet', engine='pyarrow', compression='snappy')
+
+    s3 = boto3.resource('s3')
+    object = s3.Object('matt-spotify-artists', 'audio-features/dt={}/top-tracks.parquet'.format(dt))
+    data = open('audio-features.parquet', 'rb')
+    object.put(Body=data)
+
+    print("Success")
+    sys.exit(0)
 
 
 def get_headers(client_id, client_secret):
