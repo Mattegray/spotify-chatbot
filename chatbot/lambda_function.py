a50911e529ff1b63a80197047ae1804c1ba9e8d5
Mattegray <hyungwoo_128@outlook.com>
2020-08-27 15:57:50 +0900

    Added path to libs

--- chatbot/lambda_function.py
+++ chatbot/lambda_function.py
@@ -1,5 +1,8 @@
 import sys
+sys.path.append('./libs')
 import logging
+import requests
+import pymysql
 
 PAGE_TOKEN = "EAAKf95XftI0BAD1rBPMRIi07ZBbIxJzajhZAcgAKalNYZApYeZBgAdQaUKVl53uMz6z7AUwvOhio7Y1VHC0cIsPZBmPCUuYF4quyycg5gOfpNSHOqZBBWJnBZBmput1F50Vh6RSIStA7loLqNBUseqqmpx6eeGXNyOWZAgQTcWX9uKo3n4OgU6VLNp6IZCdCbT6MZD"
 VERIFY_TOKEN = "verify_123"
